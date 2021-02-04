619f210b966157e4e3dde63b43be45147506ffa7
Kingston <kingston@vanlaak.cc>
2021-02-04 14:46:34 +0100

    fixed over loop

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index(){
-        $categories = \App\Category::all();
+        $categories = category::all();
+
+        dd($categories);
 
         return view('category.index', ['categories' => $categories]);
     }
--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -5,6 +5,8 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
 
+        
+
             @foreach($categories as $cat)
 
                 <div class="card my-4">
