f105ad294703ca87eac3cfac2fc9e18ffc6463a7
Kingston <kingston@vanlaak.cc>
2021-06-02 10:08:00 +0200

    login go to home

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/', 'HomeController@index')->name('home');
+
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/category', 'CategoryController@index');
